9bf6054c5cff86f9f8f217a48735d915313c1019
Keegan Leary <leary.keegan@gmail.com>
2022-09-23 18:47:59 -0700

    fade from left

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { splitIntoLetters } from "./split";
+
+gsap.registerPlugin(ScrollTrigger);
 
 // TEXT ANIMATIONS FOR NOCODERS
 
@@ -7,16 +10,18 @@ declare type Props = {
   duration: number;
   stagger: number;
   ease: string;
+  textDivision: string;
 };
 
 // CONSTANTS
 const ATTRIBUTE_PROP = "wb-text-animate";
 enum ATTRIBUTE_VALUES {
   STAGGER_TEXT = "stagger-text",
+  FADE_IN_FROM_LEFT = "fade-in-from-left",
 }
 
 // SELECTORS
-let elementsToAnimate: NodeList = document.querySelectorAll(
+let elementsToAnimate: NodeListOf<Element> = document.querySelectorAll(
   `[${ATTRIBUTE_PROP}]`
 );
 
@@ -25,23 +30,42 @@ const getCustomProps = (element: Element): Props => {
   let duration = Number(element.getAttribute("duration")) || 0.3;
   let stagger = Number(element.getAttribute("stagger")) || 0.1;
   let ease = element.getAttribute("ease") || "power2.out";
+  let textDivision = element.getAttribute("text-division") || "chars";
   return {
     duration,
     stagger,
     ease,
+    textDivision,
   };
 };
 
+// ANIMATE LOOP
 elementsToAnimate.forEach((el) => {
-  let attributeValue = (el as Element).getAttribute("wb-text-animate");
+  let attributeValue = el.getAttribute("wb-text-animate");
   if (attributeValue === ATTRIBUTE_VALUES.STAGGER_TEXT) {
+    let { duration, stagger, ease, textDivision } = getCustomProps(
+      el as Element
+    );
     const splitText = splitIntoLetters(el);
-    //console.log(splitText);
-    let { duration, stagger, ease } = getCustomProps(el as Element);
 
-    gsap.from(splitText.chars, {
+    gsap.from(splitText[textDivision], {
+      scrollTrigger: el,
       opacity: 0,
       y: "50%",
+      duration,
+      stagger,
+      ease,
+    });
+  }
+
+  if (attributeValue === ATTRIBUTE_VALUES.FADE_IN_FROM_LEFT) {
+    const splitText = splitIntoLetters(el);
+    let { duration, stagger, ease, textDivision } = getCustomProps(
+      el as Element
+    );
+    gsap.from(splitText[textDivision], {
+      scrollTrigger: el,
+      opacity: 0,
       duration,
       stagger,
       ease,
